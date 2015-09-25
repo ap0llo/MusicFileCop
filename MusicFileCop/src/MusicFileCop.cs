b764227e1050ec2b78acc1cc9ed51fa8b40eae48
Andreas Gruenwald <andreasgruenwald@live.de>
2015-09-25 19:06:44 +0200

    Added basic application workflow, trying to tie the pieces together

---
+++ MusicFileCop/src/MusicFileCop.cs
@@ -0,0 +1,60 @@
+﻿using MusicFileCop.Model;
+using MusicFileCop.Model.Configuration;
+using MusicFileCop.Model.FileSystem;
+using MusicFileCop.Model.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicFileCop
+{
+    class MusicFileCop
+    {
+
+        readonly IFileSystemLoader m_FileSystemLoader;
+        readonly IConfigurationLoader m_ConfigLoader;
+        readonly IMetadataLoader m_MetadataLoader;
+        readonly IConsistencyChecker m_ConsistencyChecker;
+        
+        public MusicFileCop(IFileSystemLoader fileSystemLoader, IConfigurationLoader configurationLoader,
+                            IMetadataLoader metadataLoader, IConsistencyChecker consistencyChecker)
+        {
+            if (fileSystemLoader == null)
+                throw new ArgumentNullException(nameof(fileSystemLoader));
+            if (configurationLoader == null)
+                throw new ArgumentNullException(nameof(configurationLoader));
+            if (metadataLoader == null)
+                throw new ArgumentNullException(nameof(metadataLoader));
+            if (consistencyChecker == null)
+                throw new ArgumentNullException(nameof(consistencyChecker));
+
+            this.m_FileSystemLoader = fileSystemLoader;
+            this.m_ConfigLoader = configurationLoader;
+            this.m_MetadataLoader = metadataLoader;
+            this.m_ConsistencyChecker = consistencyChecker;
+        }
+
+
+        public void Run(string directory)
+        {
+            //TODO: check if directory exists
+
+            // load file system
+            var rootDirectory = m_FileSystemLoader.LoadDirectory(directory);
+
+            //load configuration
+            //TODO: Get default configuration
+            m_ConfigLoader.LoadConfiguration(rootDirectory);
+
+            //load media metadata
+            m_MetadataLoader.LoadMetadata(rootDirectory);
+
+            m_ConsistencyChecker.CheckConsistency(rootDirectory);
+
+        }
+
+
+    }
+}
--- MusicFileCop/src/Program.cs
+++ MusicFileCop/src/Program.cs
@@ -11,23 +11,15 @@ namespace MusicFileCop
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
 
-            var path = @"C:\Users\Andreas\Desktop\01 - Prelude.mp3";
+            //get instance of Program using ninject
+            
 
-            using (var file = new AudioFile(path))
-            {
-                var tag = file.GetTag(TagTypes.Id3v2);
-
-                Console.WriteLine("Title: {0}", tag.Title);
-                Console.WriteLine("AlbumArtist: {0}", tag.AlbumArtists.FirstOrDefault());
-                Console.WriteLine("Artist: {0}", tag.Performers.FirstOrDefault());
-                Console.WriteLine("Album: {0}", tag.Album);
-            }
-
-
-            Console.ReadLine();
+           
         }
     }
 }
