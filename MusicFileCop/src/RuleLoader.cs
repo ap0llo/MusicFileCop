a086c13e509ec63e9fb39a49a8e12069d79dbafd
Andreas Gruenwald <andreasgruenwald@live.de>
2015-09-29 20:45:59 +0200

    RuleLoader will now now configure Ninject to use a specific PrefixConfigurationMapper as IMapper for rule classes

--- MusicFileCop/src/RuleLoader.cs
+++ MusicFileCop/src/RuleLoader.cs
@@ -1,16 +1,14 @@
-﻿using MusicFileCop.Model;
-using MusicFileCop.Model.DI;
-using MusicFileCop.Model.Metadata;
-using MusicFileCop.Model.Rules;
-using MusicFileCop.Rules;
-using Ninject;
-using Ninject.Extensions.Conventions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using MusicFileCop.Model;
+using MusicFileCop.Model.DI;
+using MusicFileCop.Model.Rules;
+using Ninject;
+using Ninject.Activation;
+using Ninject.Extensions.Conventions;
+using Ninject.Parameters;
 
 namespace MusicFileCop
 {
@@ -30,34 +28,49 @@ namespace MusicFileCop
         }
 
 
-
         public void LoadAllRules()
         {
-
             // get all checkables
             var ruleTypes = GetCheckableTypes()
-                .Select(checkableType => typeof(IRule<>).MakeGenericType(checkableType))
+                .Select(checkableType => typeof (IRule<>).MakeGenericType(checkableType))
                 .ToArray();
 
             m_Kernel.Bind(x =>
                 x.FromAssembliesMatching(s_RulesAssemblyName)
-                .SelectAllClasses()
-                .InheritedFromAny(ruleTypes)
-                .BindAllInterfaces());
-            
+                    .SelectAllClasses()
+                    .InheritedFromAny(ruleTypes)
+                    .BindAllInterfaces()
+                );
+
+            var ruleImplementingTypes = ruleTypes
+                .SelectMany(t => m_Kernel.GetAll(t))
+                .Select(instance => instance.GetType())
+                .ToArray();
+
+            foreach (var type in ruleImplementingTypes)
+            {
+                m_Kernel.Bind<IMapper>()
+                    .ToMethod(c => GetMapperForRule(c, type))
+                    .WhenInjectedInto(type);
+            }
         }
 
 
-
-        internal IEnumerable<Type> GetCheckableTypes() => GetCheckableTypes(Assembly.GetAssembly(typeof(ModelModule)));
+        internal IEnumerable<Type> GetCheckableTypes() => GetCheckableTypes(Assembly.GetAssembly(typeof (ModelModule)));
 
         internal IEnumerable<Type> GetCheckableTypes(Assembly assembly)
         {
             return assembly.GetTypes()
-                .Where(t => typeof(ICheckable).IsAssignableFrom(t))
+                .Where(t => typeof (ICheckable).IsAssignableFrom(t))
                 .Where(t => t.IsPublic);
         }
 
 
+        IMapper GetMapperForRule(IContext context, Type ruleImplemetationType)
+        {
+            return context.Kernel.Get<PrefixConfigurationMapper>(
+                new ConstructorArgument("settingsPrefix", ruleImplemetationType.Name)
+                );
+        }
     }
 }
