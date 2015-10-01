e15b1cecdd4bf9f7ba8152fe2168ee5539c125f8
Andreas Gruenwald <andreasgruenwald@live.de>
2015-10-01 21:15:21 +0200

    Added rule "DiskNumberIsPositive"

---
+++ MusicFileCop.Rules/src/Rules/DiskNumberIsPositive.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicFileCop.Model.Metadata;
+using MusicFileCop.Model.Rules;
+
+namespace MusicFileCop.Rules
+{
+    public class DiskNumberIsPostiveRule : IRule<IDisk>
+    {
+        public string Description => "Disk numbers must not have the value 0";
+
+        public bool IsApplicable(IDisk item) => true;
+
+        public bool IsConsistent(IDisk item) => item.DiskNumber > 0;
+        
+    }
+}
